6ab70f4080ba567060b5c31f36a0723947162c62
Md. Raisul Islam Rahad <118774591+Raisulll@users.noreply.github.com>
2023-10-15 20:54:46 +0600

    added Trie code

--- Assignment/2_202214052.cpp
+++ Assignment/2_202214052.cpp
@@ -1,226 +1,227 @@
 #include <bits/stdc++.h>
 using namespace std;
-class node
+class Node
 {
 public:
-    int key, val;
-    node()
+    int c;
+    Node *child[58];
+    Node()
     {
-        key = 0, val = 0;
-    }
-    node(int k, int v)
-    {
-        key = k, val = v;
+        c = 0;
+        for (int i = 0; i < 58; ++i)
+            child[i] = 0;
     }
 };
-class Heap
+void Trie_insert(string st, Node *root)
 {
-private:
-    node a[101];
-    int size;
-
-public:
-    Heap()
+    for (int i = 0; i < st.size(); ++i)
     {
-        size = 0;
-    }
-
-private:
-    void bottomTopAdjust(int i)
-    {
-        while (i > 1)
+        int r = st[i] - 'A';
+        if (root->child[r] == NULL)
         {
-            int child = a[i].key;
-            int parent = a[i / 2].key;
-            if (child > parent)
-                swap(a[i], a[i / 2]);
-            else
-                break;
-            i /= 2;
+            root->child[r] = new Node;
+        }
+        root = root->child[r];
+    }
+    root->c++;
+}
+int Trie_search(Node *root, string st, int k = 0)
+{
+    Node *u = root;
+    if (u == NULL)
+        return 0;
+    if (k == st.size())
+        return u->c;
+    int r = st[k] - 'A';
+    Trie_search(u->child[r], st, ++k);
+}
+void print_trie(Node *u, string st = "")
+{
+    if (u->c > 0)
+    {
+        for (int i = 0; i < u->c; ++i)
+            cout << st << endl;
+    }
+    for (int i = 0; i < 58; ++i)
+    {
+        if (u->child[i] != NULL)
+        {
+            char ch = i + 'A';
+            print_trie(u->child[i], st + ch);
         }
     }
-    void topBottomAdjust(int i)
+}
+void printRevTRIE(Node *u, string st = "")
+{
+    for (int i = 57; i >= 0; --i)
     {
-        int lc = 2 * i, rc = lc + 1;
-        if (lc == size)
+        if (u->child[i] != NULL)
         {
-            if (a[lc].key > a[i].key)
-            {
-                swap(a[lc], a[i]);
-                topBottomAdjust(lc);
-            }
+            char ch = i + 'A';
+            printRevTRIE(u->child[i], st + ch);
         }
-        else if (rc <= size)
-        {
-            int m;
-            if (a[lc].key > a[i].key)
-                m = lc;
-            else
-                m = i;
-            if (a[rc].key > a[m].key)
-                m = rc;
-            if (m != i)
-            {
-                swap(a[m], a[i]);
-                topBottomAdjust(m);
-            }
-        }
-        else
-            return;
     }
-
-public:
-    bool insert(int key, int val)
+    if (u->c > 0)
     {
-        if (size == 100)
+        for (int i = 0; i < u->c; ++i)
+            cout << st << endl;
+    }
+}
+void printDup(Node *u, string st = "")
+{
+    if (u->c > 1)
+    {
+        cout << st << endl;
+    }
+    for (int i = 0; i < 58; ++i)
+    {
+        if (u->child[i] != NULL)
+        {
+            char ch = i + 'A';
+            printDup(u->child[i], st + ch);
+        }
+    }
+}
+bool isLeaf(Node *u)
+{
+    for (int i = 0; i < 58; ++i)
+    {
+        if (u->child[i] != NULL)
             return false;
-        node n(key, val);
-        a[++size] = n;
-        bottomTopAdjust(size);
+    }
+    return true;
+}
+bool isJunc(Node *u)
+{
+    int c = 0;
+    for (int i = 0; i < 58; ++i)
+    {
+        if (u->child[i] != NULL)
+            c++;
+    }
+    if (c > 1 or u->c > 0)
         return true;
-    }
-    bool increaseKey(int val, int key)
+    return false;
+}
+void delEdge(Node *u, char c, int d)
+{
+    if (!d)
+        return;
+    int r = c - 'A';
+    Node *n = u->child[r];
+    u->child[r] = NULL;
+    delete n;
+}
+int delTrie(Node *u, string s, int k = 0)
+{
+    if (u == NULL)
+        return 0;
+    if (k == s.size())
     {
-        if (!size)
-            return false;
-        else
-        {
-            for (int i = 1; i <= size; ++i)
-            {
-                if (a[i].val == val and a[i].key < key)
-                {
-                    a[i].key = key;
-                    bottomTopAdjust(i);
-                    return true;
-                }
-            }
-            return false;
-        }
+        if (u->c == 0)
+            return 0;
+        u->c--;
+        return 0;
     }
-    pair<int, int> showMax()
+    int r = s[k] - 'A';
+    int d = delTrie(u, s, ++k);
+    int j = 0;
+    if (isJunc(u))
+        j = 1;
+    delEdge(u, s[k], d);
+    if (j)
+        d = 0;
+    return d;
+}
+Node *findptr(string s, Node *root)
+{
+    Node *u = root;
+    for (int k = 0; k < s.size(); k++)
     {
-        pair<int, int> p;
-        p.first = a[1].key;
-        p.second = a[1].val;
-        return p;
+        int r = s[k] - 65;
+        u = u->child[r];
     }
-    int showSize()
-    {
-        return size;
-    }
-    void deleteHead()
-    {
-        swap(a[1], a[size]);
-        size--;
-        topBottomAdjust(1);
-    }
-    pair<int, int> extractMax()
-    {
-        pair<int, int> p;
-        p.first = a[1].key;
-        p.second = a[1].val;
-        deleteHead();
-        return p;
-    }
-    void bfs()
-    {
-        if (size == 0)
-            return;
-        int level = 2;
-        queue<int> q;
-        q.push(1);
-        while (!q.empty())
-        {
-            int parent = q.front();
-            q.pop();
-            if (parent == level)
-            {
-                cout << endl;
-                level = level * 2;
-            }
-            cout << "{" << a[parent].key << " " << a[parent].val << "}"
-                 << " ";
-            if (2 * parent <= size)
-                q.push(2 * parent);
-            if (2 * parent + 1 <= size)
-                q.push(2 * parent + 1);
-        }
-    }
-};
+    return u;
+}
 int main()
 {
-
-    Heap heap;
-
+    Node *root = new Node();
     while (1)
     {
-        cout << "1. Insert    2. Increase Key    3. Show Max    4. Extract Max  5. Level Order Traversal 6. End" << endl
+        cout << "1. Insert    2. Search    3. Delete   4. Lexicographical Sorting  5. Display Strings  6. Reverse Sort 7. Print String Appears more than 1. 8. Print String with a specific prefix. 9. End" << endl
              << endl;
         int choice;
+        string x;
         cin >> choice;
-
         if (choice == 1)
         {
-            cout << "Insert Key: ";
-            int x;
+            cout << "Insert String: ";
             cin >> x;
-            cout << "Insert Value: ";
-            int y;
-            cin >> y;
-            bool b = heap.insert(x, y);
-
-            if (b)
-                cout << y << " is inserted in the heap" << endl;
+            Trie_insert(x, root);
+            cout << x << " is inserted in the trie" << endl;
         }
         else if (choice == 2)
         {
-            cout << "Which node's key you want to increase?" << endl;
-            int nodeNo;
-            cin >> nodeNo;
-            cout << "What will be the new key value?" << endl;
-            int keyV;
-            cin >> keyV;
-            bool b = heap.increaseKey(nodeNo, keyV);
-            if (b)
-                cout << "Node's key value increased successfully!" << endl;
+            cout << "Enter string to search: ";
+            cin >> x;
+            if (Trie_search(root, x) > 0)
+                cout << x << " FOUND " << endl;
             else
-                cout << "Unsuccessful Operation :(" << endl;
+                cout << x << " NOT FOUND " << endl;
         }
-
         else if (choice == 3)
         {
-            if (heap.showSize() != 0)
-            {
-                pair<int, int> p = heap.showMax();
-                cout << "Key->" << p.first << " Value->" << p.second << endl;
-            }
-            else
-                cout << "No element in the queue" << endl;
+            cout << "Enter string to delete: ";
+            cin >> x;
+            delTrie(root, x, 0);
         }
-
         else if (choice == 4)
         {
-            if (heap.showSize() != 0)
-            {
-                pair<int, int> p = heap.extractMax();
-                cout << "Key->" << p.first << " Value->" << p.second << endl;
-            }
-            else
-                cout << "No element in the queue" << endl;
+            print_trie(root);
         }
-
         else if (choice == 5)
         {
-            cout << "Level Wise Traversal of the Queue:" << endl;
-            heap.bfs();
-            cout << endl;
+            print_trie(root);
         }
         else if (choice == 6)
+        {
+            printRevTRIE(root);
+        }
+        else if (choice == 7)
+        {
+            printDup(root);
+        }
+        else if (choice == 8)
+        {
+            cout << "Enter the Prefix" << endl;
+            cin >> x;
+            Node *u = findptr(x, root);
+            print_trie(u, x);
+        }
+        else if (choice == 9)
+        {
             break;
+        }
         else
         {
             cout << "Invalid Choice" << endl;
+            break;
         }
         cout << endl;
     }
 }
+/*inputs:
+
+1 MIST
+1 MIT
+1 CSE
+1 CSE
+1 CE
+1 MISTCSE
+1 ME
+1 EECE
+1 ABC
+1 ABC
+1 AB
+
+*/
--- test.cpp
+++ test.cpp
@@ -1,115 +1,227 @@
 #include <bits/stdc++.h>
 using namespace std;
-void BellmanFord(int n, char src, vector<vector<pair<char, int>>> &edges, vector<int> &dist, vector<char> &parent)
+class Node
 {
-    dist[src - 'A' + 1] = 0;
-
-    for (int i = 0; i < n; i++) // n-1 times relaxation
+public:
+    int c;
+    Node *child[58];
+    Node()
     {
-        for (int j = 1; j <= n; j++) // iterates through each vertex
-        {
-            for (int k = 0; k < edges[j].size(); k++) // iterates through all the outgoing edges of the currrent vertex j
-            {
-                char u = j + 'A' - 1;
-                char v = edges[j][k].first;
-                int wt = edges[j][k].second;
-
-                if (dist[j] != INT_MAX && ((dist[j] + wt) < dist[v - 'A' + 1]))
-                {
-                    dist[v - 'A' + 1] = dist[j] + wt;
-                    parent[v - 'A' + 1] = u;
-                }
-            }
-        }
+        c = 0;
+        for (int i = 0; i < 58; ++i)
+            child[i] = 0;
     }
-
-    int flag = 0;
-    for (int j = 1; j <= n; j++) // iterates through each vertex
-    {
-        for (int k = 0; k < edges[j].size(); k++) // iterates through all the outgoing edges of the currrent vertex j
-        {
-            char u = j + 'A' - 1;
-            char v = edges[j][k].first;
-            int wt = edges[j][k].second;
-
-            if (dist[j] != INT_MAX && ((dist[j] + wt) < dist[v - 'A' + 1]))
-            {
-                flag = 1;
-            }
-        }
-    }
-    if (flag == 0)
-    {
-        cout << "NO NEG CYCLE" << endl;
-        cout << "Vertex distances: " << endl;
-        for (int i = 1; i <= n; i++)
-        {
-            cout << char('A' + i - 1) << ": " << dist[i] << endl;
-        }
-    }
-    else
-        cout << "NEG CYCLE FOUND" << endl;
-}
-void printPath(int n, char src, char dest, vector<int> &dist, vector<char> &parent)
+};
+void Trie_insert(string st, Node *root)
 {
-    vector<char> path;
-    char cur = dest;
-    while (cur != src)
+    for (int i = 0; i < st.size(); ++i)
     {
-        path.push_back(cur);
-        cur = parent[cur - 'A' + 1];
+        int r = st[i] - 'A';
+        if (root->child[r] == NULL)
+        {
+            root->child[r] = new Node;
+        }
+        root = root->child[r];
     }
-    path.push_back(src);
-    reverse(path.begin(), path.end());
-    cout << "Shortest path from " << src << " to " << dest << " is: ";
-    for (int i = 0; i < path.size(); i++)
-    {
-        cout << path[i] << " ";
-    }
-    cout << endl;
+    root->c++;
 }
-
+int Trie_search(Node *root, string st, int k = 0)
+{
+    Node *u = root;
+    if (u == NULL)
+        return 0;
+    if (k == st.size())
+        return u->c;
+    int r = st[k] - 'A';
+    Trie_search(u->child[r], st, ++k);
+}
+void print_trie(Node *u, string st = "")
+{
+    if (u->c > 0)
+    {
+        for (int i = 0; i < u->c; ++i)
+            cout << st << endl;
+    }
+    for (int i = 0; i < 58; ++i)
+    {
+        if (u->child[i] != NULL)
+        {
+            char ch = i + 'A';
+            print_trie(u->child[i], st + ch);
+        }
+    }
+}
+void printRevTRIE(Node *u, string st = "")
+{
+    for (int i = 57; i >= 0; --i)
+    {
+        if (u->child[i] != NULL)
+        {
+            char ch = i + 'A';
+            printRevTRIE(u->child[i], st + ch);
+        }
+    }
+    if (u->c > 0)
+    {
+        for (int i = 0; i < u->c; ++i)
+            cout << st << endl;
+    }
+}
+void printDup(Node *u, string st = "")
+{
+    if (u->c > 1)
+    {
+        cout << st << endl;
+    }
+    for (int i = 0; i < 58; ++i)
+    {
+        if (u->child[i] != NULL)
+        {
+            char ch = i + 'A';
+            printDup(u->child[i], st + ch);
+        }
+    }
+}
+bool isLeaf(Node *u)
+{
+    for (int i = 0; i < 58; ++i)
+    {
+        if (u->child[i] != NULL)
+            return false;
+    }
+    return true;
+}
+bool isJunc(Node *u)
+{
+    int c = 0;
+    for (int i = 0; i < 58; ++i)
+    {
+        if (u->child[i] != NULL)
+            c++;
+    }
+    if (c > 1 or u->c > 0)
+        return true;
+    return false;
+}
+void delEdge(Node *u, char c, int d)
+{
+    if (!d)
+        return;
+    int r = c - 'A';
+    Node *n = u->child[r];
+    u->child[r] = NULL;
+    delete n;
+}
+int delTrie(Node *u, string s, int k = 0)
+{
+    if (u == NULL)
+        return 0;
+    if (k == s.size())
+    {
+        if (u->c == 0)
+            return 0;
+        u->c--;
+        return 0;
+    }
+    int r = s[k] - 'A';
+    int d = delTrie(u, s, ++k);
+    int j = 0;
+    if (isJunc(u))
+        j = 1;
+    delEdge(u, s[k], d);
+    if (j)
+        d = 0;
+    return d;
+}
+Node *findptr(string s, Node *root)
+{
+    Node *u = root;
+    for (int k = 0; k < s.size(); k++)
+    {
+        int r = s[k] - 65;
+        u = u->child[r];
+    }
+    return u;
+}
 int main()
 {
-    int v, e;
-    cout << "No of vertices: ";
-    cin >> v;
-    cout << "No of edges: ";
-    cin >> e;
-    vector<int> dist(v + 1, INT_MAX);
-    vector<char> parent(v + 1, '\0');
-    set<char> st;
-    vector<vector<pair<char, int>>> adj(v + 1);
-    for (int i = 0; i < e; i++)
+    Node *root = new Node();
+    while (1)
     {
-        char u, v;
-        int wt;
-        cin >> u >> v >> wt;
-        adj[u - 'A' + 1].push_back({v, wt});
-        st.insert(u);
-        st.insert(v);
-    }
-    char src;
-    cout << "Enter source node: ";
-    cin >> src;
-    BellmanFord(v, src, adj, dist, parent);
-    for (auto it : st)
-    {
-        if (it != src)
+        cout << "1. Insert    2. Search    3. Delete   4. Lexicographical Sorting  5. Display Strings  6. Reverse Sort 7. Print String Appears more than 1. 8. Print String with a specific prefix. 9. End" << endl
+             << endl;
+        int choice;
+        string x;
+        cin >> choice;
+        if (choice == 1)
         {
-            printPath(v, src, it, dist, parent);
+            cout << "Insert String: ";
+            cin >> x;
+            Trie_insert(x, root);
+            cout << x << " is inserted in the trie" << endl;
         }
+        else if (choice == 2)
+        {
+            cout << "Enter string to search: ";
+            cin >> x;
+            if (Trie_search(root, x) > 0)
+                cout << x << " FOUND " << endl;
+            else
+                cout << x << " NOT FOUND " << endl;
+        }
+        else if (choice == 3)
+        {
+            cout << "Enter string to delete: ";
+            cin >> x;
+            delTrie(root, x, 0);
+        }
+        else if (choice == 4)
+        {
+            print_trie(root);
+        }
+        else if (choice == 5)
+        {
+            print_trie(root);
+        }
+        else if (choice == 6)
+        {
+            printRevTRIE(root);
+        }
+        else if (choice == 7)
+        {
+            printDup(root);
+        }
+        else if (choice == 8)
+        {
+            cout << "Enter the Prefix" << endl;
+            cin >> x;
+            Node *u = findptr(x, root);
+            print_trie(u, x);
+        }
+        else if (choice == 9)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Invalid Choice" << endl;
+            break;
+        }
+        cout << endl;
     }
 }
-/*
-5
-7
-A B 3
-A C 1
-B D 1
-B E 2
-C B 1
-C D 4
-D E 3
-A
+/*inputs:
+
+1 MIST
+1 MIT
+1 CSE
+1 CSE
+1 CE
+1 MISTCSE
+1 ME
+1 EECE
+1 ABC
+1 ABC
+1 AB
+
 */
