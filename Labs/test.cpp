29048cfe7184275431d9ff376bafda6fc9886d38
Md. Raisul Islam Rahad <118774591+Raisulll@users.noreply.github.com>
2023-10-23 12:50:01 +0600

    sos added

---
+++ Labs/test.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> ans;
+vector<int> arr;
+int n;
+
+void fun(int in, int s)
+{
+    if (s == 0)
+    {
+        for (auto it : ans)
+            cout << it << " ";
+        cout << endl;
+        return;
+    }
+    if (in == n)
+        return;
+    if (arr[in] <= s)
+    {
+        ans.push_back(arr[in]);
+        fun(in + 1, s - arr[in]);
+        ans.pop_back();
+    }
+    fun(in + 1, s); // Consider subsets without the current element
+}
+
+int main()
+{
+    cin >> n;
+    arr.resize(n);
+    for (auto &it : arr)
+        cin >> it;
+    int sum;
+    cin >> sum;
+    fun(0, sum);
+    return 0;
+}
